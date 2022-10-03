91819afe4f94b6e539a5ffbcf0eb5b541ea6f209
BreakTheMyth <96642694+BreakTheMyth@users.noreply.github.com>
2022-10-03 21:03:53 +0800

    Add files via upload

---
+++ a.js
@@ -0,0 +1,21 @@
+var start =  document.getElementById("start");
+
+start.onclick = function() {
+    if(document.getElementById("ka") != null) {
+        document.getElementById("ka").remove();
+    }
+    var num = Math.trunc(1 + Math.random()*79);
+    start.style.top = "90%";
+    var ka = document.createElement("div");
+    ka.style.backgroundImage = "url(res/" + num +".png)";
+    ka.style.width = "50vw";
+    ka.style.height = "50vw";
+    ka.style.backgroundSize = "cover";
+    ka.style.borderRadius = "1000px";
+    ka.style.position = "absolute";
+    ka.style.left = "50%";
+    ka.style.top = "20%";
+    ka.style.transform = "translateX(-50%)", "translateY(-50%)";
+    ka.id = "ka";
+    document.body.appendChild(ka);
+}
